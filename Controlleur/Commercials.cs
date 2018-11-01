14aa2a7b4690221994329f32179147bd6ff60137
Aurelie Chassagne <aurelie.chassagne@viacesi.fr>
2018-11-01 17:30:03 +0100

    Ajout du controlleur et du modele du commercial

---
+++ Controlleur/Commercials.cs
@@ -0,0 +1,99 @@
+﻿using Madera.Modele;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madera.Controlleur
+{
+    class Commercials
+    { 
+        private static ConnexionBDD connexion = new ConnexionBDD();
+        public static List<Commercial> chargerCommercial()
+        {
+            List<Commercial> lesCommercials = new List<Commercial>();
+            try
+            {
+                SqlDataReader reader;
+                reader = connexion.execRead("SELECT NomCommercial from Commercials");
+                if (reader.Read())
+                {
+                    while (reader.Read())
+                    {
+                        Commercial c = new Commercial(
+                            reader.GetInt32(0), 
+                            reader.GetString(1), 
+                            reader.GetString(2), 
+                            reader.GetString(3),
+                            reader.GetString(4));
+                        lesCommercials.Add(c);
+                    }
+                }
+                reader.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+            }
+            return lesCommercials;
+        }
+        public static Boolean CreerCommercial(Commercial commercial)
+        {
+            Boolean test = false;
+            try
+            {
+                connexion.execWrite("INSERT INTO Commercials" +
+                    "(Nom, Prenom, Login, Password) " +
+                    "VALUES ('"
+                    + commercial.Nom + "', '"
+                    + commercial.Prenom + "', '"
+                    + commercial.Login + "', '"
+                    + commercial.Password + "');");
+                test = true;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+                test = false;
+            }
+            return test;
+        }
+        public static Boolean ModifierCommercial(Commercial commercial)
+        {
+            Boolean test = false;
+            try
+            {
+                connexion.execWrite("UPDATE Commercials Id = '" + commercial.Id + "'," +
+                    " Nom = '" + commercial.Nom + "'," +
+                    " Prenom = '" + commercial.Prenom + "', " +
+                    " Login = '" + commercial.Login + "', " +
+                    " Password = '" + commercial.Password + "' ;");
+                test = true;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+                test = false;
+            }
+            return test;
+        }
+        public static Boolean SupprimerCommercial(Commercial commercial)
+        {
+            bool test = false;
+            try
+            {
+                connexion.execWrite("DELETE FROM Commercial WHERE Id = "
+                    + commercial.Id + " ;");
+                test = true;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+                test = false;
+            }
+            return test;
+        }
+    }
+}
---
+++ Modele/Commercial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madera.Modele
+{
+    class Commercial
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Commercial(int id, string nom, string prenom, string login, string password)
+        {
+            this.Id = id;
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Login = login;
+            this.Password = password;
+        }
+    }
+}

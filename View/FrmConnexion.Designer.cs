c7f41adcb562a7af8ddb6b26a3c6c2c5d650c525
Célian <celian.picot01@gmail.com>
2019-04-08 14:37:10 +0200

    Modification LoginView (checkbox 'Se souvenir de moi', ...)

--- View/FrmConnexion.Designer.cs
+++ View/FrmConnexion.Designer.cs
@@ -30,15 +30,19 @@
         {
             this.grpConnexion = new System.Windows.Forms.GroupBox();
             this.btnConnexion = new System.Windows.Forms.Button();
-            this.tbxIdentifiant = new System.Windows.Forms.TextBox();
-            this.lblIdentifiant = new System.Windows.Forms.Label();
             this.lblMdp = new System.Windows.Forms.Label();
             this.tbxMdp = new System.Windows.Forms.TextBox();
+            this.lblIdentifiant = new System.Windows.Forms.Label();
+            this.tbxIdentifiant = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.chkRememberMe = new System.Windows.Forms.CheckBox();
             this.grpConnexion.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpConnexion
             // 
+            this.grpConnexion.Controls.Add(this.chkRememberMe);
             this.grpConnexion.Controls.Add(this.btnConnexion);
             this.grpConnexion.Controls.Add(this.lblMdp);
             this.grpConnexion.Controls.Add(this.tbxMdp);
@@ -47,7 +51,7 @@
             this.grpConnexion.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grpConnexion.Location = new System.Drawing.Point(12, 77);
             this.grpConnexion.Name = "grpConnexion";
-            this.grpConnexion.Size = new System.Drawing.Size(380, 200);
+            this.grpConnexion.Size = new System.Drawing.Size(380, 261);
             this.grpConnexion.TabIndex = 0;
             this.grpConnexion.TabStop = false;
             this.grpConnexion.Text = "Connexion";
@@ -55,29 +59,13 @@
             // btnConnexion
             // 
             this.btnConnexion.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConnexion.Location = new System.Drawing.Point(234, 164);
+            this.btnConnexion.Location = new System.Drawing.Point(234, 225);
             this.btnConnexion.Name = "btnConnexion";
             this.btnConnexion.Size = new System.Drawing.Size(140, 30);
             this.btnConnexion.TabIndex = 2;
-            this.btnConnexion.Text = "Se connecter";
+            this.btnConnexion.Text = "&Se connecter";
             this.btnConnexion.UseVisualStyleBackColor = true;
             this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
-            // 
-            // tbxIdentifiant
-            // 
-            this.tbxIdentifiant.Location = new System.Drawing.Point(124, 56);
-            this.tbxIdentifiant.Name = "tbxIdentifiant";
-            this.tbxIdentifiant.Size = new System.Drawing.Size(250, 27);
-            this.tbxIdentifiant.TabIndex = 0;
-            // 
-            // lblIdentifiant
-            // 
-            this.lblIdentifiant.AutoSize = true;
-            this.lblIdentifiant.Location = new System.Drawing.Point(6, 59);
-            this.lblIdentifiant.Name = "lblIdentifiant";
-            this.lblIdentifiant.Size = new System.Drawing.Size(91, 20);
-            this.lblIdentifiant.TabIndex = 1;
-            this.lblIdentifiant.Text = "Identifiant :";
             // 
             // lblMdp
             // 
@@ -96,16 +84,74 @@
             this.tbxMdp.Size = new System.Drawing.Size(250, 27);
             this.tbxMdp.TabIndex = 1;
             // 
+            // lblIdentifiant
+            // 
+            this.lblIdentifiant.AutoSize = true;
+            this.lblIdentifiant.Location = new System.Drawing.Point(6, 59);
+            this.lblIdentifiant.Name = "lblIdentifiant";
+            this.lblIdentifiant.Size = new System.Drawing.Size(91, 20);
+            this.lblIdentifiant.TabIndex = 1;
+            this.lblIdentifiant.Text = "Identifiant :";
+            // 
+            // tbxIdentifiant
+            // 
+            this.tbxIdentifiant.Location = new System.Drawing.Point(124, 56);
+            this.tbxIdentifiant.Name = "tbxIdentifiant";
+            this.tbxIdentifiant.Size = new System.Drawing.Size(250, 27);
+            this.tbxIdentifiant.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MADERA";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(362, 9);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(30, 30);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // chkRememberMe
+            // 
+            this.chkRememberMe.AutoSize = true;
+            this.chkRememberMe.Location = new System.Drawing.Point(10, 160);
+            this.chkRememberMe.Name = "chkRememberMe";
+            this.chkRememberMe.Size = new System.Drawing.Size(165, 24);
+            this.chkRememberMe.TabIndex = 4;
+            this.chkRememberMe.Text = "Se souvenir de moi";
+            this.chkRememberMe.UseVisualStyleBackColor = true;
+            // 
             // FrmConnexion
             // 
+            this.AcceptButton = this.btnConnexion;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 381);
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(400, 350);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.grpConnexion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmConnexion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FrmConnexion_Load);
             this.grpConnexion.ResumeLayout(false);
             this.grpConnexion.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -117,5 +163,8 @@
         private System.Windows.Forms.TextBox tbxMdp;
         private System.Windows.Forms.Label lblIdentifiant;
         private System.Windows.Forms.TextBox tbxIdentifiant;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.CheckBox chkRememberMe;
     }
 }
